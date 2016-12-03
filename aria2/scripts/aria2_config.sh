e98db3ca1073c84e32bca5668f7af99166027122
lixin <sadoneli@gmail.com>
2016-12-03 11:48:44 +0800

    fix aria2

---
+++ aria2/scripts/aria2_config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+eval `dbus export aria2`
+
+if [ "$aria2_enable" == "1" ];then
+	sh /koolshare/aria2/aria2_run.sh restart
+else
+	sh /koolshare/aria2/aria2_run.sh stop
+fi
---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-11-23_16:12:15", 
+    "description": "linux下载利器", 
+    "home_url": "Module_aria2.asp", 
+    "md5": "fbb3fd101d8f3fa0d8eeda9bda9ccf90", 
+    "module": "aria2", 
+    "title": "Aria2", 
+    "version": "1.7.6"
+}
