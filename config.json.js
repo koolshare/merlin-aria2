31b19a33146629316801f2db879dfea39272ee00
lixin <sadoneli@gmail.com>
2017-05-17 11:13:36 +0800

    update aria2

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-11-23_16:12:15", 
     "description": "linux下载利器", 
     "home_url": "Module_aria2.asp", 
-    "md5": "c6b912ab1d909a1ba203e958b2c9cac0", 
+    "md5": "3417430efdb318a76a2a1d1fa8fbb700", 
     "module": "aria2", 
     "title": "Aria2", 
-    "version": "1.7.7"
+    "version": "1.7.8"
 }
