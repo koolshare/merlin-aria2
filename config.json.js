12f036249d362685ef2266915db0bba4bd7bebca
sadoneli <sadoneli@gmail.com>
2017-09-26 09:07:31 +0800

    update aria2 to adapt x7.7

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-11-23_16:12:15", 
     "description": "linux下载利器", 
     "home_url": "Module_aria2.asp", 
-    "md5": "746d4699410e0f7d2e460ab298a61876", 
+    "md5": "5f92dd5c496ef9924f5f5b4a06ce3e12", 
     "module": "aria2", 
     "title": "Aria2", 
-    "version": "1.7.9"
+    "version": "1.8.0"
 }
